66d6bb7f13346d9543b27a56b7f3e088c89fb4e8
Jiadong Liu <liujiadong111@gmail.com>
2024-04-19 19:14:12 -0700

    clean proj

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -4,7 +4,7 @@ import { ModuleInfo } from './index.d'
 export const WEEK = ['周日', '周一', '周二', '周三', '周四', '周五', '周六']
 
 // 主题名称与副标题名称
-export const title = '上海旅游可视化平台'
+export const title = '旅游大数据可视化系统'
 export const subtitle = ['', '', '']
 
 export const moduleInfo: ModuleInfo = [
