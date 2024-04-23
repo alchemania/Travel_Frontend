02467b5f96dc40c8f62b16d88ae80fb98fd5170d
williamliu <liujiadong111@gmail.com>
2024-04-23 15:58:57 -0700

    v2.0 clean version

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,17 @@ import './assets/scss/style.scss';
 import './assets/icon/iconfont.css'
 // 引入 全局注册组件
 import PublicComponent from '@/components/componentInstall';
-// 引入element-plus
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-//引入vue-pro-table
-import Vue3ProTable from "vue3-pro-table";
+// // 引入element-plus
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+// //引入vue-pro-table
+// import Vue3ProTable from "vue3-pro-table";
 
 const app = createApp(App)
 app.use(PublicComponent)
 app.use(DataVVue3)
-app.use(store)
+// app.use(store)
 app.use(router)
-app.use(ElementPlus)
-app.use(Vue3ProTable)
+// app.use(ElementPlus)
+// app.use(Vue3ProTable)
 app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
+    productionSourceMap: false,
     transpileDependencies: false,
     lintOnSave: false,
-    assetsDir:'static'
+    assetsDir: 'static'
 })
+
