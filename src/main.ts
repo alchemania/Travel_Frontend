cb01cf51d3c84816c1664c62832c1f445b81418c
williamliu <liujiadong111@gmail.com>
2024-05-09 20:23:42 -0700

    v2.0.2 remove vuex

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import DataVVue3 from '@kjgl77/datav-vue3'
 // 引入全局css
 import './assets/scss/style.scss';
@@ -9,17 +8,9 @@ import './assets/scss/style.scss';
 import './assets/icon/iconfont.css'
 // 引入 全局注册组件
 import PublicComponent from '@/components/componentInstall';
-// // 引入element-plus
-// import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
-// //引入vue-pro-table
-// import Vue3ProTable from "vue3-pro-table";
 
 const app = createApp(App)
 app.use(PublicComponent)
 app.use(DataVVue3)
-// app.use(store)
 app.use(router)
-// app.use(ElementPlus)
-// app.use(Vue3ProTable)
 app.mount('#app')
